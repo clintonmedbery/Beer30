98f7de661033523dc767d683689166e3a7577d1c
Clinton Medbery <clinton.medbery@thinkideally.com>
2016-04-02 01:50:33 -0400

    Initial Commit

---
+++ Beer30/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beer30
+//
+//  Created by Clinton Medbery on 4/2/16.
+//  Copyright © 2016 Programadores Sans Frontieres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
